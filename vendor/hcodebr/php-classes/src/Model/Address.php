224588d0372e74ccf9d89fc0bab1b793857d5a78
CARLOS <carloshilariu@gmail.com>
2019-11-05 14:11:20 -0400

    Gerar boleto

--- vendor/hcodebr/php-classes/src/Model/Address.php
+++ vendor/hcodebr/php-classes/src/Model/Address.php
@@ -56,7 +56,7 @@ class Address extends Model {
 
 		$sql = new Sql();
 
-		$results = $sql->select("CALL sp_address_save(:idaddress, :idperson, :desaddresss, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", [
+		$results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddresss, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", [
 			'idaddress'=>$this->getidaddress(),
 			'idperson'=>$this->getidperson(),
 			'desaddresss'=>utf8_decode($this->getdesaddresss()),
